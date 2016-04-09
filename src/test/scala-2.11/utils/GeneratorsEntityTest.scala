f718e72a0bc57b1c675bec5837acf605c7e8d2b4
Alexander <branevskij_aj@bw-sw.com>
2016-04-09 16:23:12 +0600

    minor refatoring GenEntityTest

--- src/test/scala-2.11/utils/GeneratorsEntityTest.scala
+++ src/test/scala-2.11/utils/GeneratorsEntityTest.scala
@@ -9,7 +9,7 @@ import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 class GeneratorsEntityTest extends FlatSpec with Matchers with BeforeAndAfterAll{
   "Generators.getTimeUUID()" should "return unique UUID" in {
     val gen = new GeneratorsEntity
-     var uniqElements = Set[UUID]()
+    var uniqElements = Set[UUID]()
     for (i <- 0 until 100) {
       val prevSize = uniqElements.size
 
